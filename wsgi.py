b9d38d3d896a6389fa67d297c9d1558ace865345
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-06-15 07:12:31 -0400

    added view

--- wsgi.py
+++ wsgi.py
@@ -3,17 +3,32 @@ import logging
 from flask import Flask, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import create_engine, Boolean, Column, Integer, MetaData, String, Table
 
+database_uri = 'postgresql://postgres:postgres@localhost:5432/flask-view-demo'
 
 app = Flask('flask-view-demo')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/flask-view-demo'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.logger.setLevel(logging.DEBUG)
 application = app
 
 db = SQLAlchemy(app)
+engine = create_engine(database_uri)
+metadata = MetaData()
+metadata.bind = engine
 migrate = Migrate(app, db)
+
+
+active_products_view = Table('active_products_view', metadata,
+    Column("id", Integer(), primary_key=True),
+    Column("name", String(32)),
+    autoload=True
+)
+
+
+class ActiveProduct(db.Model):
+    __table__ = active_products_view
 
 
 class Product(db.Model):
@@ -37,11 +52,11 @@ def seed_db():
 
 
 def serialize_product(product:Product) -> dict:
-    return {
-        'id': product.id,
-        'name': product.name,
-        'active': product.active
-    }
+    d = {}
+    for attr in ['id', 'name', 'active']:
+        if hasattr(product, attr):
+            d[attr] = getattr(product, attr)
+    return d
 
 
 @app.route('/')
@@ -51,8 +66,15 @@ def route_index():
 
 
 @app.route('/products')
-def route_product():
+def route_products():
     products = Product.query.all()
+    products_dict = [serialize_product(product) for product in products]
+    return jsonify(products_dict)
+
+
+@app.route('/products/active')
+def route_products_active():
+    products = ActiveProduct.query.all()
     products_dict = [serialize_product(product) for product in products]
     return jsonify(products_dict)
 
